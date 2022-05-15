5f02dec7498754b6e514222aa2b40d96368a3f73
dleodns1591 <88140817+dleodns1591@users.noreply.github.com>
2022-05-15 16:07:41 +0900

    타미어 및 체력
    타미어 및 체력

---
+++ Assets/LeeDaeWoon/Script/Ingame/HP_Bar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP_Bar : MonoBehaviour
+{
+    public GameObject Bar;
+
+    void Start()
+    {
+        InvokeRepeating("Test", 0.5f, 0.5f);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Test()
+    {
+        Debug.Log("0.5 °¨¼Ò");
+        Bar.transform.localScale += new Vector3(0, -0.1f, 0);
+    }
+}
