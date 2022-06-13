13639363fb0eb08fa7bda629e2a36fb1d6c70029
Black_Penguin <dkstmdwnswns@gmail.com>
2022-06-14 02:49:22 +0900

    ObjectPool

--- Assets/Black_Penguin/Scripts/ObjectPool.cs
+++ Assets/Black_Penguin/Scripts/ObjectPool.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ObjectPool<T> : MonoBehaviour
+public class ObjectPoolClass
 {
-    static public ObjectPool<T> Instance;
+    public GameObject parentObj;
+    public Queue<GameObject> objQueue = new Queue<GameObject>();
+}
+
+public class ObjectPool : MonoBehaviour
+{
+    static public ObjectPool Instance;
+    public Dictionary<GameObject, ObjectPoolClass> ParentObj;
     public GameObject[] objects;
     public void Awake()
     {
@@ -16,6 +22,54 @@ public class ObjectPool<T> : MonoBehaviour
         else
         {
             Destroy(this.gameObject);
+        }
+    }
+    public GameObject CreateObj(GameObject obj)
+    {
+        if (ParentObj[obj].parentObj == null)
+        {
+            GameObject newObj = new GameObject();
+            newObj.transform.parent = gameObject.transform;
+            ParentObj[obj].parentObj = newObj;
+
+            Instantiate(obj, ParentObj[obj].parentObj.transform);
+        }
+        else
+        {
+            if (ParentObj[obj].objQueue.Count > 0)
+            {
+                GameObject returnObj = ParentObj[obj].objQueue.Dequeue();
+                returnObj.SetActive(true);
+                return returnObj;
+            }
+            else
+            {
+                GameObject returnObj = Instantiate(obj, ParentObj[obj].parentObj.transform);
+                return returnObj;
+            }
+        }
+        return null;
+    }
+    public GameObject CreateObj(GameObject obj, Vector3 pos, Quaternion quaternion)
+    {
+        GameObject returnObj = CreateObj(obj);
+        returnObj.transform.position = pos;
+        returnObj.transform.rotation = quaternion;
+
+        return returnObj;
+    }
+    public void DeleteObj(GameObject obj)
+    {
+        if (ParentObj[obj].parentObj != null)
+        {
+            ParentObj[obj].objQueue.Enqueue(obj);
+            obj.gameObject.SetActive(false);
+            obj.transform.position = Vector3.zero;
+        }
+        else
+        {
+            Debug.Assert(false);
+            Debug.Log("오브젝트풀 오류 발생 (본 객체는 오브젝트풀 객체가 아닙니다)");
         }
     }
 }
