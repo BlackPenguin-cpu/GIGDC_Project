6868678fe675584a3ba53747510c2d9b4aa10288
dleodns1591 <88140817+dleodns1591@users.noreply.github.com>
2022-06-14 22:37:47 +0900

    리소스 덜 추가
    리소스 덜 추가

--- Assets/LeeDaeWoon/Script/Ingame/HP_Bar.cs
+++ Assets/LeeDaeWoon/Script/Ingame/HP_Bar.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class HP_Bar : MonoBehaviour
 {
+    public float HP;
     public GameObject Bar;
 
     void Start()
     {
-        InvokeRepeating("Test", 0.5f, 0.5f);
     }
 
     void Update()
     {
-
     }
 
     public void Test()
     {
-        //Debug.Log("0.5 °¨¼Ò");
-        if (Bar.transform.localScale.y >= 0.001)
-        {
-            Bar.transform.localScale += new Vector3(0, -0.01f, 0);
-        }
+        //if (Bar.transform.localScale.y >= 0.001)
+        //{
+        //    Bar.transform.localScale += new Vector3(0, -0.01f, 0);
+        //}
+
 
     }
 }
