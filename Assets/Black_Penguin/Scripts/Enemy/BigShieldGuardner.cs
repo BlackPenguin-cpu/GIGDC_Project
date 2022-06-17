621f80080a81545b82a2d01473012b422398b88f
BlackPenguin <dkstmdwnswns@naver.com>
2022-06-17 20:39:09 +0900

    Player Anim Processing

--- Assets/Black_Penguin/Scripts/Enemy/BigShieldGuardner.cs
+++ Assets/Black_Penguin/Scripts/Enemy/BigShieldGuardner.cs
@@ -4,33 +4,49 @@ using UnityEngine;
 
 public class BigShieldGuardner : BaseEnemy
 {
+    AttackCollision collision;
     protected override void Start()
     {
-        BaseStatSet(80, 30, 1, 18, 0, 0, 0, 0);
+        collision = transform.GetComponentInChildren<AttackCollision>();
+        BaseStatSet(500, 30, 3, 4, 48, 55, 40, 55);
+
         base.Start();
     }
     protected override void Update()
     {
         base.Update();
-        if (PlayerDetect() && curAttackDelay > attackDelay)
+        if (collision.isCanAttack(this) && curAttackDelay > attackDelay)
         {
             curAttackDelay = 0;
         }
     }
-    bool PlayerDetect()
+    public override void Die()
     {
-        //임의로 넣은 수 (나중에 기획 제대로 나오면 수정 필요)
-        float distance = collider.size.x * 1;
+        base.Die();
+    }
 
-        RaycastHit2D[] rays = Physics2D.BoxCastAll(transform.position, collider.size, 0, Vector2.right);
-        foreach (var detectObj in rays)
+    public IEnumerator TakleAttack()
+    {
+        //.해당 값은 상수처리
+        float forcePower = 10;
+        Vector3 dir = sprite.flipX ? Vector3.left : Vector3.right;
+
+        for (int i = 0; i < 100; i++)
         {
-            if (detectObj.transform.TryGetComponent(out Player player)
-                && player.transform.position.x - gameObject.transform.position.x < distance)
+            transform.Translate(dir * forcePower);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    void TakleAttackCheck()
+    {
+        RaycastHit2D[] rays = Physics2D.BoxCastAll(transform.position, collider.size, 0, Vector2.right, 0.01f);
+        foreach (RaycastHit2D ray in rays)
+        {
+            if (ray.transform.TryGetComponent(out Player player))
             {
-                return true;
+                Attack(player, attackDamage);
             }
         }
-        return false;
+
     }
 }
