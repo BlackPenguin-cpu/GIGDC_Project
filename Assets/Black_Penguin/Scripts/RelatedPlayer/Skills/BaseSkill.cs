35fc5c6111c12cabade6746d6c820ff66a6256ae
BlackPenguin <dkstmdwnswns@naver.com>
2022-07-01 08:00:05 +0900

    skill little change

--- Assets/Black_Penguin/Scripts/RelatedPlayer/Skills/BaseSkill.cs
+++ Assets/Black_Penguin/Scripts/RelatedPlayer/Skills/BaseSkill.cs
@@ -7,8 +7,6 @@ public abstract class BaseSkill : MonoBehaviour, IObjectPoolingObj
     public DimensionType dimensionType = DimensionType.NONE;
     public SkillScript SkillInfo;
     public float StartPosY;
-    public float curCooldown;
-    public bool canUseSkill;
 
     protected SpriteRenderer sprite;
     protected Player player;
@@ -34,15 +32,6 @@ public abstract class BaseSkill : MonoBehaviour, IObjectPoolingObj
     public float DefaultReturnDamage()
     {
         return SkillInfo.damagePercent / 100 * player.stat._attackDamage;
-    }
-    protected virtual void Update()
-    {
-        curCooldown += Time.deltaTime;
-        Mathf.Clamp(curCooldown, 0, SkillInfo._cooldown);
-        if (curCooldown >= SkillInfo._cooldown)
-        {
-            canUseSkill = true;
-        }
     }
     protected abstract void Action();
 }
