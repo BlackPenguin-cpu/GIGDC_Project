f4031018b4705b5e3646e98880374bdf49ea491a
Black_Penguin <dkstmdwnswns@gmail.com>
2022-07-07 01:54:50 +0900

    again Fix

--- Assets/Black_Penguin/Scripts/Manager/ObjectPool.cs
+++ Assets/Black_Penguin/Scripts/Manager/ObjectPool.cs
@@ -125,6 +125,10 @@ public class ObjectPool : MonoBehaviour
 
     private void OnLevelWasLoaded(int level)
     {
+        CleanChildObj();
+    }
+    void CleanChildObj()
+    {
         for (int i = 0; i < transform.childCount; i++)
         {
             Destroy(transform.GetChild(i));
